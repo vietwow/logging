740b72ba1cba431a3299d11cc45c5448f3a6cca8
Viet Nguyen <vietwow@gmail.com>
2019-02-21 16:31:40 +0700

    fix

--- main.go
+++ main.go
@@ -11,18 +11,6 @@ func main() {
     topic := os.Getenv("TOPIC") // heroku_logs
     broker := os.Getenv("BROKER") // localhost:29092
 
-    // Initialize kafka producer
-    err := producer.InitKafka(broker)
-    if err != nil {
-        fmt.Printf("Failed to create producer: %s\n", err)
-        os.Exit(1)
-    }
-
-    message := "Hello Go!"
-
-    producer.Produce(topic, message)
-
-
     // Initialize kafka consumer
     err = consumer.InitKafka(broker)
     if err != nil {
