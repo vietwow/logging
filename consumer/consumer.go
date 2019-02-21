c1a926fbf1c08d0745bcfcfc00452322eff7eba1
Viet Nguyen <vietwow@gmail.com>
2019-02-21 16:50:22 +0700

    fix

--- consumer/consumer.go
+++ consumer/consumer.go
@@ -20,12 +20,6 @@ func InitKafka(broker string) error {
     c, err = kafka.NewConsumer(&kafka.ConfigMap{
         "bootstrap.servers":               broker,
         "group.id":                        group,
-        "session.timeout.ms":              6000,
-        "go.events.channel.enable":        true,
-        "go.application.rebalance.enable": true,
-        // Enable generation of PartitionEOF when the
-        // end of a partition is reached.
-        "enable.partition.eof": true,
         "auto.offset.reset":    "earliest"})
 
     return err
@@ -44,31 +38,13 @@ func Consume(topic string) {
         fmt.Println("subscribed to topic ", topic)
     }
 
-    run := true
-
-    for run == true {
-        select {
-        case sig := <-sigchan:
-            fmt.Printf("Caught signal %v: terminating\n", sig)
-            run = false
-
-        case ev := <-c.Events():
-            switch e := ev.(type) {
-            case kafka.AssignedPartitions:
-                fmt.Fprintf(os.Stderr, "%% %v\n", e)
-                c.Assign(e.Partitions)
-            case kafka.RevokedPartitions:
-                fmt.Fprintf(os.Stderr, "%% %v\n", e)
-                c.Unassign()
-            case *kafka.Message:
-                fmt.Printf("%% Message on %s:\n%s\n",
-                    e.TopicPartition, string(e.Value))
-            case kafka.PartitionEOF:
-                fmt.Printf("%% Reached %v\n", e)
-            case kafka.Error:
-                // Errors should generally be considered as informational, the client will try to automatically recover
-                fmt.Fprintf(os.Stderr, "%% Error: %v\n", e)
-            }
+    for {
+        msg, err := c.ReadMessage(-1)
+        if err == nil {
+            fmt.Printf("Message on %s: %s\n", msg.TopicPartition, string(msg.Value))
+        } else {
+            // The client will automatically try to recover from all errors.
+            fmt.Printf("Consumer error: %v (%v)\n", err, msg)
         }
     }
 
