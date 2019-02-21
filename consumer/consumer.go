37d12e6085f5cdd1cc3db718ab6917fbf0563fd0
Viet Nguyen <vietwow@gmail.com>
2019-02-21 22:37:41 +0700

    fix

--- consumer/consumer.go
+++ consumer/consumer.go
@@ -31,7 +31,7 @@ func Consume(topic string) {
     sigchan := make(chan os.Signal, 1)
     signal.Notify(sigchan, os.Interrupt)
 
-    err := c.SubscribeTopics([]string{topic, "^aRegex.*[Tt]opic"}, nil)
+    err := c.SubscribeTopics([]string{topic, nil)
     if err != nil {
         fmt.Println("Unable to subscribe to topic " + topic + " due to error - " + err.Error())
         os.Exit(1)
