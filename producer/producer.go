1e5d6940403b2d60d5ce762a84499ae2e36c6d81
Viet Nguyen <vietwow@gmail.com>
2019-02-21 16:17:01 +0700

    fix

--- producer/producer.go
+++ producer/producer.go
@@ -18,33 +18,26 @@ func InitKafka(broker string) error {
 func Produce(topic string, message string) {
     fmt.Printf("=> Created Producer %v\n", p)
 
-    doneChan := make(chan bool)
-
-    go func() {
-        defer close(doneChan)
-        for e := range p.Events() {
-            switch ev := e.(type) {
-            case *kafka.Message:
-                m := ev
-                if m.TopicPartition.Error != nil {
-                    fmt.Printf("Delivery failed: %v\n", m.TopicPartition.Error)
-                } else {
-                    fmt.Printf("Delivered message to topic %s [%d] at offset %v\n",
-                        *m.TopicPartition.Topic, m.TopicPartition.Partition, m.TopicPartition.Offset)
-                }
-                return
-
-            default:
-                fmt.Printf("Ignored event: %s\n", ev)
-            }
-        }
-    }()
+    // Optional delivery channel, if not specified the Producer object's
+    // .Events channel is used.
+    deliveryChan := make(chan kafka.Event)
 
     value := "Hello Go!"
-    p.ProduceChannel() <- &kafka.Message{TopicPartition: kafka.TopicPartition{Topic: &topic, Partition: kafka.PartitionAny}, Value: []byte(value)}
+    err = p.Produce(&kafka.Message{
+        TopicPartition: kafka.TopicPartition{Topic: &topic, Partition: kafka.PartitionAny},
+        Value:          []byte(value),
+        Headers:        []kafka.Header{{Key: "myTestHeader", Value: []byte("header values are binary")}},
+    }, deliveryChan)
 
-    // wait for delivery report goroutine to finish
-    _ = <-doneChan
+    e := <-deliveryChan
+    m := e.(*kafka.Message)
 
-    p.Close()
+    if m.TopicPartition.Error != nil {
+        fmt.Printf("Delivery failed: %v\n", m.TopicPartition.Error)
+    } else {
+        fmt.Printf("Delivered message to topic %s [%d] at offset %v\n",
+            *m.TopicPartition.Topic, m.TopicPartition.Partition, m.TopicPartition.Offset)
+    }
+
+    close(deliveryChan)
 }
