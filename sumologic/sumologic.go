94773a79aa439b0c7f50f1695b8918e03fbbecfb
Viet Nguyen <vietwow@gmail.com>
2019-02-25 11:45:49 +0700

    update

--- sumologic/sumologic.go
+++ sumologic/sumologic.go
@@ -155,7 +155,7 @@ func (s *SumoLogic) SendLogs(logStringToSend string) {
 		} else if response.StatusCode == 200 {
 			// logging.Trace.Println("Post of logs successful")
 			logging.Info.Println("dc roai")
-			logging.Error.Println("dc roai")
+			// logging.Error.Println("dc roai")
 			fmt.Println("Post of logs successful")
 			s.timerBetweenPost = time.Now()
 		}
